8f6d2b4c0f3971899ff8cf978d135ead5ada00e2
Timothy Cheung <tcheung@gatech.com>
2023-03-07 16:46:55 -0500

    edit

--- model1/gather_data.py
+++ model1/gather_data.py
@@ -2,15 +2,15 @@ import sagemaker
 import boto3
 import pandas as pd
 import os
+import io
 
 BUCKET = "circleci-sagemaker"
 
 role = os.environ['SAGEMAKER_EXECUTION_ROLE']
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-print(aws_access_key_id)
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 region_name = "us-east-1"
-prefix = "xgboost-churn"
+model_name = "xgboost-churn"
 
 
 boto_session = boto3.Session(
@@ -23,13 +23,10 @@ s3_client = boto_session.client(service_name="s3")
 s3_client.download_file(f"sagemaker-sample-files", "datasets/tabular/synthetic/churn.txt", "churn.txt")
 churn = pd.read_csv("./churn.txt")
 
-churn.to_csv("train.csv", header=False, index=False)
 
 
-s3_client.Bucket(BUCKET).Object(
-    f"{prefix}/data/train.csv"
-).upload_file("train.csv")
+csv_buffer = io.BytesIO()
+churn.to_csv(csv_buffer, index=False)
 
-# boto3.Session().resource("s3").Bucket(bucket).Object(
-#     os.path.join(prefix, "validation/validation.csv")
-# ).upload_file("validation.csv")
+s3_client.put_object(Bucket=BUCKET, Body=csv_buffer.getvalue(), Key=f"{model_name}/data/train.csv")
+
