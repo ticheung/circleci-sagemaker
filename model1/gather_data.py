5a3089b98c2180033eb62353131f8d55af991681
Timothy Cheung <tcheung@gatech.com>
2023-03-07 16:25:38 -0500

    env variables

--- model1/gather_data.py
+++ model1/gather_data.py
@@ -6,10 +6,15 @@ import os
 BUCKET = "circleci-sagemaker"
 
 role = os.environ['SAGEMAKER_EXECUTION_ROLE']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 prefix = "xgboost-churn"
 
 
-boto_session = boto3.Session()
+boto_session = boto3.Session(
+    # aws_access_key_id = 
+    # aws_secret_access_key =
+)
 s3_client = boto_session.client(service_name="s3")
 
 s3_client.download_file(f"sagemaker-sample-files", "datasets/tabular/synthetic/churn.txt", "churn.txt")
