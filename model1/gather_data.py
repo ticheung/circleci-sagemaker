650792038e856b190a73d85122807b563b912ed3
Timothy Cheung <tcheung@gatech.com>
2023-03-07 21:30:56 -0500

    add model training

--- model1/gather_data.py
+++ model1/gather_data.py
@@ -1,32 +1,46 @@
 import sagemaker
 import boto3
 import pandas as pd
+import numpy as np
 import os
 import io
 
-BUCKET = "circleci-sagemaker"
-
-# role = os.environ['SAGEMAKER_EXECUTION_ROLE']
-# aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-# aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-region_name = "us-east-1"
-model_name = "xgboost-churn"
+bucket = os.environ["AWS_BUCKET"]
+region_name = os.environ["AWS_REGION"]
+model_name = os.environ["MODEL_NAME"]
 
 
-boto_session = boto3.Session(
-    # aws_access_key_id = aws_access_key_id,
-    # aws_secret_access_key = aws_secret_access_key,
-    region_name = region_name
-)
+# Set up the session and client we will need for this step
+boto_session = boto3.Session(region_name=region_name)
 s3_client = boto_session.client(service_name="s3")
 
+
+# Data retrieval and processing taken from
+# https://github.com/aws/amazon-sagemaker-examples/blob/main/introduction_to_applying_machine_learning/xgboost_customer_churn/xgboost_customer_churn.ipynb
+# You would likely replace this part for your own use case, such as querying from Snowflake
 s3_client.download_file(f"sagemaker-sample-files", "datasets/tabular/synthetic/churn.txt", "churn.txt")
 churn = pd.read_csv("./churn.txt")
 
+churn = churn.drop("Phone", axis=1)
+churn["Area Code"] = churn["Area Code"].astype(object)
+churn = churn.drop(["Day Charge", "Eve Charge", "Night Charge", "Intl Charge"], axis=1)
 
+model_data = pd.get_dummies(churn)
+model_data = pd.concat(
+    [model_data["Churn?_True."], model_data.drop(["Churn?_False.", "Churn?_True."], axis=1)], axis=1
+)
+
+train_data, validation_data, test_data = np.split(
+    model_data.sample(frac=1, random_state=1729),
+    [int(0.7 * len(model_data)), int(0.9 * len(model_data))],
+)
+
+
+# Upload training and validation data to S3
+csv_buffer = io.BytesIO()
+train_data.to_csv(csv_buffer, index=False)
+s3_client.put_object(Bucket=bucket, Body=csv_buffer.getvalue(), Key=f"{model_name}/train/train.csv")
 
 csv_buffer = io.BytesIO()
-churn.to_csv(csv_buffer, index=False)
-
-s3_client.put_object(Bucket=BUCKET, Body=csv_buffer.getvalue(), Key=f"{model_name}/data/train.csv")
-
+validation_data.to_csv(csv_buffer, index=False)
+s3_client.put_object(Bucket=bucket, Body=csv_buffer.getvalue(), Key=f"{model_name}/validation/validation.csv")
---
+++ model1/train_model.py
@@ -0,0 +1,67 @@
+import sagemaker
+from sagemaker.estimator import Estimator
+from sagemaker.inputs import TrainingInput
+import boto3
+import pandas as pd
+import numpy as np
+import os
+import io
+
+bucket = os.environ["AWS_BUCKET"]
+region_name = os.environ["AWS_REGION"]
+model_name = os.environ["MODEL_NAME"]
+role_arn = os.environ["SAGEMAKER_EXECUTION_ROLE_ARN"]
+
+
+# Set up the session and client we will need for this step
+boto_session = boto3.Session(region_name=region_name)
+sagemaker_client = boto_session.client(service_name="sagemaker")
+sagemaker_runtime_client = boto_session.client(service_name="sagemaker-runtime")
+sagemaker_session = sagemaker.Session(
+    boto_session = boto_session,
+    sagemaker_client = sagemaker_client,
+    sagemaker_runtime_client = sagemaker_runtime_client,
+    default_bucket = bucket
+)
+# s3_client = boto_session.client(service_name="s3")
+
+# Set up dataset locations
+train_set_location = f"s3://{bucket}/{model_name}/train/"
+validation_set_location = f"s3://{bucket}/{model_name}/validation/"
+model_location = f"s3://{bucket}/{model_name}/model/"
+
+train_set_pointer = TrainingInput(s3_data=train_set_location, content_type='csv')
+validation_set_pointer = TrainingInput(s3_data=validation_set_location, content_type='csv')
+
+# Retrieve xgboost image
+image_uri = sagemaker.image_uris.retrieve(
+    framework = "xgboost",
+    region = region_name,
+    version = "1.5-1"
+)
+
+# Configure training estimator
+xgb_estimator = Estimator(
+    base_job_name = model_name,
+    image_uri = image_uri,
+    instance_type = "ml.m5.medium",
+    instance_count = 1,
+    output_path = model_location,
+    sagemaker_session = sagemaker_session,
+    role = role_arn,
+    hyperparameters = {
+        "objective": "binary:logistic",
+        "max_depth": 5,
+        "eta": 0.2,
+        "gamma": 4,
+        "min_child_weight": 6,
+        "subsample": 0.8,
+        "verbosity": 0,
+        "num_round": 100,
+    }
+)
+
+xgb_estimator.fit({"train": train_set_pointer, "validation": validation_set_pointer})
+
+training_job_name = xgb_estimator.latest_training_job.job_name
+print("training_job_name:", training_job_name)
